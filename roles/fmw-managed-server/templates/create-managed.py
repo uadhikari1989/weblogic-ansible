464ce0840dedd4233911b5b6014dd204bb55de63
Umesh Adhikari <42251216+uadhikari1989@users.noreply.github.com>
2019-10-06 15:15:05 -0500

    Update create-managed.py
    adapt script to use multiple servers

--- roles/fmw-managed-server/templates/create-managed.py
+++ roles/fmw-managed-server/templates/create-managed.py
@@ -4,7 +4,7 @@ connect('{{ weblogic_admin }}', '{{ weblogic_admin_pass }}', ADMIN_SERVER_URL);
 
 edit();
 startEdit();
-# applyJRF(target='{{ managed_server_name }}', domainDir='{{ domain_home }}');
+# applyJRF(target='{{ managed_server[0].name }}', domainDir='{{ domain_home }}');
 cd('/')
 cmo.createMachine('{{ server_hostname }}')
 
@@ -12,16 +12,18 @@ cd('/Machines/' + '{{ server_hostname }}' + '/NodeManager/' + '{{ server_hostnam
 cmo.setListenAddress('{{ node_manager_listen_address }}')
 cmo.setListenAddress('{{ node_manager_listen_address }}')
 
-cd('/')
-cmo.createServer('{{ managed_server_name }}')
+{% for host in managed_server %}
+  cd('/')
+  cmo.createServer('{{ host.name }}')
 
-cd('/Servers/' + '{{ managed_server_name }}')
-cmo.setListenAddress('0.0.0.0')
-cmo.setListenPort({{ managed_server_port }})
-cmo.setMachine(getMBean('/Machines/' + '{{ server_hostname }}'))
-# applyJRF(target='{{ managed_server_name }}', domainDir='{{ domain_home }}');
+  cd('/Servers/' + '{{ host.name }}')
+  cmo.setListenAddress('0.0.0.0')
+  cmo.setListenPort({{ host.port }})
+  cmo.setMachine(getMBean('/Machines/' + '{{ host.name }}'))
+  # applyJRF(target='{{ managed_server[0].name }}', domainDir='{{ domain_home }}');
 
-# applyJRF wil call save and activate
-save();
-activate(block='true');
-disconnect();
+  # applyJRF wil call save and activate
+  save();
+  activate(block='true');
+  disconnect();
+{% endfor %}
